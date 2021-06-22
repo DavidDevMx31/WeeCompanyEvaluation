cc15a826828fec22277c19ad9aab0c2781d8fe1b
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2021-06-22 11:27:55 -0500

    Elimina storyboard y asigna rootViewController

---
+++ PruebaTecnica/Paises/View/PaisesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PaisesViewController.swift
+//  PruebaTecnica
+//
+//  Created by David Ali on 22/06/21.
+//
+
+import UIKit
+
+class PaisesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Países"
+        view.backgroundColor = .blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
